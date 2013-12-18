c40f74644137e777be65ebde851411ed2b326bb1
wilhelmson <wilhelmson@el-xps.(none)>
2013-12-17 20:07:01 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = function(fn, fnLength) {
+	var slice = Array.prototype.slice;
+	
+	fnLength = fnLength || fn.length;
+
+	return function makeCurry (){
+		var args = slice.call(arguments);
+		if(args.length === fnLength) return fn.apply(this, args);
+		return function(){
+			var newArgs = slice.call(arguments);
+			return makeCurry.apply(this, args.concat(newArgs));
+		}
+	}
+}
